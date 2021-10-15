38452455fa3ff66aae6711530e7131555ab6b6bc
nika kotrikadze <nikakotrikadze12@gmail.com>
2021-10-15 22:09:12 +0400

    dababycommit

---
+++ main.js
@@ -0,0 +1,57 @@
+function DaBaby() {
+  document.getElementById("daBabyPic").style.display = "block";
+  document.getElementById("ReloadP").style.display = "block";
+  document.getElementById("letsGoo").style.display = "block";
+  DaBabyAudio();
+}
+
+function DaBabyAudio() {
+  var audio = new Audio("audio/daBaby.mp3");
+  audio.oncanplaythrough = function () {
+    audio.play();
+    document.getElementById("daBabyButton").style.display = "none";
+    document.getElementById("lilPumpButton").style.display = "none";
+  };
+}
+
+function LilPumpAudio() {
+  var audio = new Audio("audio/lilPump.mp3");
+  audio.oncanplaythrough = function () {
+    audio.play();
+    document.getElementById("daBabyButton").style.display = "none";
+    document.getElementById("lilPumpButton").style.display = "none";
+  };
+}
+
+// function DoAgain() {
+//   document.getElementById("daBabyPic").style.display = "none";
+//   document.getElementById("ReloadP").style.display = "none";
+//   document.getElementById("letsGoo").style.display = "none";
+//   document.getElementById("daBabyButton").style.display = "block";
+//   document.getElementById("lilPumpButton").style.display = "block";
+// }
+
+function FakePump() {
+  DaBaby();
+  document.getElementById("actualPump").style.display = "block";
+}
+
+function ActualPump() {
+  document.getElementById("lilPumpPic").style.display = "block";
+  document.getElementById("ReloadP").style.display = "block";
+  document.getElementById("lilPumpButton").style.display = "none";
+  document.getElementById("actualPump").style.display = "none";
+  document.getElementById("daBabyPic").style.display = "none";
+  document.getElementById("ReloadP").style.display = "none";
+  document.getElementById("letsGoo").style.display = "none";
+  LilPumpAudio();
+}
+
+if (ActualPump()) {
+  FakePump().play();
+} else {
+  ActualPump().pause();
+}
+
+// var daBabyAudio = document.getElementById("daBabyAudio");
+// var lilPumpAudio = document.getElementById("lilPumpAudio");
